8e0fb05936720db6e52b50449c400f3fefc5d6c6
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-09-30 15:35:28 -0400

    Create pset2.js

---
+++ pset2.js
@@ -0,0 +1,26 @@
+// MAKE SURE YOU CONSOLE LOG EVERY ANSWER TO ENSURE 
+// YOU'RE DOING IT CORRECTLY (please)
+
+// pleae create:
+// a variable called `helloWrold`, you can set it equal to whatever you want
+
+// a variable called `this_is_snake_cased`, you can set it equal to whatever you want
+
+
+// a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
+
+
+// 1. create a string - it should be your first name
+
+
+// 2. create another string - it should be your last name
+
+
+// 3. create a third string using the first two strings you've defined
+// it should read [FIRST NAME][SPACE][LAST NAME]
+
+// 4. create a string - it should be your middle name
+
+
+// 5. redefine the string in step 3. so that it now shows 
+// [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
