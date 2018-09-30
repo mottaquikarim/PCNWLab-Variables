d3ebe3a0ebbda59281d3583d2b85cc76a58175f2
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-09-30 15:35:17 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+// Hi, let's describe your characteristics
+
+/*
+    P1:
+    Create a variable called `myFullName` 
+    that stores your full name as a variable
+
+*/
+
+
+// -------------------------------------------
+/*
+    P2:
+    Create a variable called `myHeight`
+    that represents your height in inches
+
+*/
+
+// -------------------------------------------
+/*
+    P3:
+    Create a variable called `myBirthYear`
+    that stores your year of birth
+
+*/
+
+// -------------------------------------------
+/*
+    P4:
+    Create a variable called `currentYear`
+    that stores current year
+
+*/
+
+// -------------------------------------------
+/*
+    P5:
+    Create a variable called `myAge`
+    that computes your current age
+    from `myBirthYear` and `curentYear`
+
+*/
+
+// -------------------------------------------
+/*
+    P6:
+    Create a string called `myDescription`
+    that incorporates the values above 
+    into a small string description about you
+    
+    ie: Hello, my name is [fullName] and I am [myHeight]...
+    etc
+    
+
+*/
+
+// -------------------------------------------
+
+
