1e75be6b8b5ab678e73b1471a7e702e48eb267b8
Pamela Abreu <pamelaabreu@pursuit.com>
2018-10-06 15:55:36 -0400

    Changed descriptions about me

--- pset1.js
+++ pset1.js
@@ -4,25 +4,24 @@
     P1:
     Create a variable called `myFullName` 
     that stores your full name as a variable
-
 */
-
+const myFullName = 'Pamela Abreu';
 
 // -------------------------------------------
 /*
     P2:
     Create a variable called `myHeight`
-    that represents your height in inches
-
+    that represents your height in inches    
 */
+const myHeight = "5'2";
 
 // -------------------------------------------
 /*
     P3:
     Create a variable called `myBirthYear`
     that stores your year of birth
-
 */
+const myBirthYear = '1996';
 
 // -------------------------------------------
 /*
@@ -31,16 +30,18 @@
     that stores current year
 
 */
-
+const currentYear = '2018';
 // -------------------------------------------
 /*
     P5:
     Create a variable called `myAge`
     that computes your current age
     from `myBirthYear` and `curentYear`
+    Solution:
+    const myAge = 'Pamela Abreu';
 
 */
-
+const myAge = (currentYear - myBirthYear)
 // -------------------------------------------
 /*
     P6:
@@ -49,10 +50,11 @@
     into a small string description about you
     
     ie: Hello, my name is [fullName] and I am [myHeight]...
-    etc
-    
-
+    etc    
 */
+let myDescription = 'Hello, my name is ' + myFullName + ' I am ' + myHeight + ' I am ' + myAge + '.';
+
+console.log(myDescription);
 
 // -------------------------------------------
 
