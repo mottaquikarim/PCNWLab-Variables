d4186d9caa3010fcfde27f186dce636d899f0151
Richard Green <richardgreen@pursuit.org>
2018-10-06 17:41:12 -0400

    answered pset2.js

--- pset2.js
+++ pset2.js
@@ -4,23 +4,35 @@
 // pleae create:
 // a variable called `helloWrold`, you can set it equal to whatever you want
 
+console.log('Rich');
+
 // a variable called `this_is_snake_cased`, you can set it equal to whatever you want
 
+let this_is_snake_cased = 'apple';
+console.log(this_is_snake_cased)
 
 // a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
 
-
+let thisVarHasNumbers111 = 222;
+console.log(thisVarHasNumbers111);
 // 1. create a string - it should be your first name
-
-
+let firstName = 'Richard';
+console.log(firstName);
 // 2. create another string - it should be your last name
-
+let lastName = 'Green';
+console.log(lastName);
 
 // 3. create a third string using the first two strings you've defined
+
+console.log(firstName + ' ' + lastName);
+
 // it should read [FIRST NAME][SPACE][LAST NAME]
 
 // 4. create a string - it should be your middle name
 
+let middleName = 'Craig'
 
 // 5. redefine the string in step 3. so that it now shows 
 // [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
+
+console.log(firstName + ' ' + middleName + ' ' + lastName);
