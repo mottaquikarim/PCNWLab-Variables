97275f064772255665c3edc7a1d79ecddcf436aa
Jennifer Perez <jenniferperez@pursuit.org>
2018-10-06 16:46:28 -0400

    my second problem set for variables

--- pset2.js
+++ pset2.js
@@ -4,23 +4,36 @@
 // pleae create:
 // a variable called `helloWrold`, you can set it equal to whatever you want
 
-// a variable called `this_is_snake_cased`, you can set it equal to whatever you want
+let helloWrold= "Earth";
+console.log (helloWrold);
 
+// a variable called `this_is_snake_cased`, you can set it equal to whatever you want
+let this_is_snake_cased= "my_name_is_jenny";
+console.log (this_is_snake_cased);
 
 // a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
-
+let thisVarHasNumbers111="111";
+console.log (thisVarHasNumbers111);
 
 // 1. create a string - it should be your first name
-
+let firstName="Jennifer";
+console.log(firstName);
 
 // 2. create another string - it should be your last name
-
+let lastName="Perez";
+console.log(lastName);
 
 // 3. create a third string using the first two strings you've defined
 // it should read [FIRST NAME][SPACE][LAST NAME]
+let name= firstName + " " + lastName;
+console.log ("My name is " + name);
 
 // 4. create a string - it should be your middle name
-
+let middleName="Marie";
+console.log ("My middle name is " + middleName);
 
 // 5. redefine the string in step 3. so that it now shows 
 // [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
+
+name = firstName + " "+ middleName + " "+lastName;
+console.log (name);
