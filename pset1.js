d37329194f54f7fcc4a7daeccaeddfe3001e630c
JayRodrig <mrjosemlrod@gmail.com>
2018-10-06 15:46:02 -0400

    fixed some bugs

--- pset1.js
+++ pset1.js
@@ -4,10 +4,10 @@
     P1:
     Create a variable called `myFullName` 
     that stores your full name as a variable
-
-    const myFullName: Jose Rodriguez
-
 */
+    const myFullName = 'Jose Rodriguez'
+
+
 
 
 // -------------------------------------------
@@ -15,30 +15,30 @@
     P2:
     Create a variable called `myHeight`
     that represents your height in inches
-
-    const myHeight: '5 feet, 10 inches'
-
 */
+    const myHeight = '5 feet, 10 inches'
+
+
 
 // -------------------------------------------
 /*
     P3:
     Create a variable called `myBirthYear`
     that stores your year of birth
-
+*/
     const myBirthyear = 1995
 
-*/
+
 
 // -------------------------------------------
 /*
     P4:
     Create a variable called `currentYear`
     that stores current year
-
+*/
     let currentYear = 2018
 
-*/
+
 
 // -------------------------------------------
 /*
@@ -46,10 +46,10 @@
     Create a variable called `myAge`
     that computes your current age
     from `myBirthYear` and `curentYear`
-
+*/
     const myAge = currentYear - myBirthyear
 
-*/
+
 
 // -------------------------------------------
 /*
@@ -60,11 +60,11 @@
     
     ie: Hello, my name is [fullName] and I am [myHeight]...
     etc
-
+*/
     myDescription = 'Hey, my name is ' + myFullName + ' and I am a Real Madrid fan. ' + 'I am ' + myAge + ' years old.'
     
+    console.log(myDescription)
 
-*/
 
 // -------------------------------------------
 
