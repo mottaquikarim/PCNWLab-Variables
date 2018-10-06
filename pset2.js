e9af90bc1ac9784ab38e8729038e05164d073199
Syed Aziz <syedaziz27x@gmail.com>
2018-10-06 16:50:30 -0400

    pset2 completed

--- pset2.js
+++ pset2.js
@@ -3,24 +3,28 @@
 
 // pleae create:
 // a variable called `helloWrold`, you can set it equal to whatever you want
-
+let helloWrold = 1
+console.log(helloWrold)
 // a variable called `this_is_snake_cased`, you can set it equal to whatever you want
-
-
+let this_is_snake_cased = 'lower_case'
+console.log(this_is_snake_cased)
 // a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
-
-
+let thisVarHasNumbers111 = 111
+console.log(thisVarHasNumbers111)
 // 1. create a string - it should be your first name
-
-
+'Syed'
+console.log('Syed')
 // 2. create another string - it should be your last name
-
-
+'Aziz'
+console.log('Aziz')
 // 3. create a third string using the first two strings you've defined
 // it should read [FIRST NAME][SPACE][LAST NAME]
+'Syed', 'Aziz'
+console.log('Syed Aziz')
 
 // 4. create a string - it should be your middle name
-
-
+'Abdul'
+console.log('Abdul')
 // 5. redefine the string in step 3. so that it now shows 
 // [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
+console.log('Syed Abdul Aziz')
