11d952d6cec1a11f7ac0a634cc871543632c7db7
Alexander <aionate0812@gmail.com>
2018-10-06 16:40:30 -0400

    solutions to pset2

--- pset2.js
+++ pset2.js
@@ -4,23 +4,29 @@
 // pleae create:
 // a variable called `helloWrold`, you can set it equal to whatever you want
 
+const helloWrold = "Yeet"
 // a variable called `this_is_snake_cased`, you can set it equal to whatever you want
 
-
+const this_is_snake_cased = "YEET!"
 // a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
 
+const thisVarHasNumbers111 = "YeeT"
 
 // 1. create a string - it should be your first name
 
-
+const firstName = "Alex"
 // 2. create another string - it should be your last name
 
+const lastName = "Onate"
 
 // 3. create a third string using the first two strings you've defined
 // it should read [FIRST NAME][SPACE][LAST NAME]
 
+let fullName = firstName+' '+lastName
 // 4. create a string - it should be your middle name
-
+const middleName = "Israel"
 
 // 5. redefine the string in step 3. so that it now shows 
 // [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
+
+fullName = firstName+' '+middleName+' '+lastName
