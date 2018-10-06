4ccf87c1ce8b3e41bdc7be98f7c4dcc68cbb593e
daniel ashley <danielashley@pursuit.org>
2018-10-06 16:04:03 -0400

    Variable PSET2

--- pset2.js
+++ pset2.js
@@ -3,24 +3,27 @@
 
 // pleae create:
 // a variable called `helloWrold`, you can set it equal to whatever you want
-
+let helloWorld = 'January'
 // a variable called `this_is_snake_cased`, you can set it equal to whatever you want
 
-
+let this_is_snake_cased = 'October'
 // a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
 
-
+let thisVarHasNumbers111 = 'February'
 // 1. create a string - it should be your first name
-
+let firstName = 'Daniel '
 
 // 2. create another string - it should be your last name
-
+let lastName = 'Ashley'
 
 // 3. create a third string using the first two strings you've defined
+let fullName = firstName + lastName
 // it should read [FIRST NAME][SPACE][LAST NAME]
 
 // 4. create a string - it should be your middle name
 
-
+let middleName = 'Eduardo '
 // 5. redefine the string in step 3. so that it now shows 
 // [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
+ fullName = firstName + middleName + lastName
+console.log(fullName);
