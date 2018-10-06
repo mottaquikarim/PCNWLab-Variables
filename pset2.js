3d31e4831735865e1bf01b6de95f94ee09a2cb74
Chuyun Huang <chuyunhuang@pursuit.org>
2018-10-06 16:40:15 -0400

    completed pset2

--- pset2.js
+++ pset2.js
@@ -3,24 +3,36 @@
 
 // pleae create:
 // a variable called `helloWrold`, you can set it equal to whatever you want
+let helloWrold = 'Hello, welcome to my website.';
+console.log(helloWrold);
 
 // a variable called `this_is_snake_cased`, you can set it equal to whatever you want
-
+let this_is_snake_cased = '..there is nothing on my website yet..'
+console.log(this_is_snake_cased);
 
 // a variable called `thisVarHasNumbers111`, you can set it equal to whatever you want
-
+let thisVarHasNumbers111 = 111;
+console.log(thisVarHasNumbers111);
 
 // 1. create a string - it should be your first name
-
+let myFirstName = 'Chuyun';
+console.log(myFirstName);
 
 // 2. create another string - it should be your last name
-
+let myLastName = 'Huang';
+console.log(myLastName);
 
 // 3. create a third string using the first two strings you've defined
+let myFullName = myFirstName + ' ' + myLastName;
+console.log(myFullName);
+
 // it should read [FIRST NAME][SPACE][LAST NAME]
 
 // 4. create a string - it should be your middle name
-
+let myMiddleName = 'Yun'
+console.log(myMiddleName);  
 
 // 5. redefine the string in step 3. so that it now shows 
 // [FIRST NAME][SPACE][MIDDLE NAME][SPACE][LAST NAME]
+myFullName= 'Chuyun Yun Huang';
+console.log('My name is'+ ' '+ myFullName);
